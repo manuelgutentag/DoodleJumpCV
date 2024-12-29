ab1e180485217a67a5e4b310cb757afa27199c85
Manuel Matheis <matheis@fzi.de>
2024-12-29 21:49:52 -0100

    multiple Platform jumping

--- doodlejump.py
+++ doodlejump.py
@@ -1,10 +1,10 @@
-import pygame, sys
+import pygame, sys, random
+from pygame.math import Vector2
 
-class Doodle():
+class Doodle:
     def __init__(self):
-        self.x_doodle = w*48/100
-        #self.y_doodle= h*3/5
-        self.y_doodle = 0
+        self.x_doodle = w * 48/100
+        self.y_doodle= h * 2/5
 
         self.x_doodle_prev = 0
         self.y_doodle_prev = 0
@@ -13,29 +13,68 @@ class Doodle():
 
         self.falling = False
         self.ascending = False
+        self.jump = False
 
         self.gravity_const = 0.7
         self.gravity_speed = 0
 
+        self.score = 0
+
+        self.list = [Vector2(w * 0.48, h * 4/5)] #create list for platforms including the starting platform
+        self.platform_rect = []
+
+        self.base_height = h * 4/5
+        self.new_height = 0
+
         self.doodle_left = pygame.image.load('Graphics/doodle_left.png').convert_alpha()
         self.doodle_right = pygame.image.load('Graphics/doodle_right.png').convert_alpha()
+        self.yellow_platform = pygame.image.load('Graphics/yellow_platform.png').convert_alpha()
+
+        for i in range(9):
+            #create platforms
+            self.list.append(Vector2(w * random.uniform(0.4, 0.6), h * (1 - i/5)))
+        for i in range(10):
+            #create platform rects
+            self.platform_rect.append(pygame.Rect(self.list[i].x, self.list[i].y, 225, 65))
+
 
     def check_jump(self):
-        #checks when doodle should jump the first platform
-        if self.y_doodle + 243 <= Entity.first_platform_y + 45 and self.y_doodle + 243 >= Entity.first_platform_y and (
-            self.x_doodle + 200 >= Entity.first_platform_x  and self.x_doodle + 50 <= Entity.first_platform_x + 225) and self.falling:
-            return True
+        self.jump = False
+        #checks when doodle should jump the other platforms
+        for i in range(10):
+            if (self.y_doodle + 243 <= self.list[i].y + 45 and self.y_doodle + 243 >= self.list[i].y and
+                (self.x_doodle + 200 >= self.list[i].x and self.x_doodle + 50 <= self.list[i].x + 225) and self.falling):
+                self.jump = True
+                self.new_height = self.list[i].y
+
+                #"scrolling" up after a successful height increase
+                if self.list[i].y < h * 4/5:
+                    for j in range(10):
+                        self.list[j].y += self.base_height - self.new_height
+                        #clear platform_rect list to render the updated platform positions
+                        self.platform_rect.pop(0)
+                    for k in range(10):
+                        self.platform_rect.append(pygame.Rect(self.list[k].x, self.list[k].y, 225, 65))
+
+                print(self.platform_rect)
+
+        return self.jump
 
     def vertical_movement(self):
         self.y_doodle_prev = self.y_doodle
         self.gravity_speed += self.gravity_const
         self.y_doodle += self.y_doodle_speed + self.gravity_speed
+        if self.ascending:
+            self.score += 1
         if self.y_doodle_prev <= self.y_doodle:
             self.falling = True
+            self.ascending = False
+        else:
+            self.ascending = True
+            self.falling = False
         if self.check_jump():
             self.y_doodle_speed = -35
             self.gravity_speed = 0
-
 
     def move_left(self):
         self.x_doodle -= 2
@@ -47,35 +86,28 @@ class Doodle():
         doodle_rect = pygame.Rect(self.x_doodle, self.y_doodle, 250, 243)
         screen.blit(self.doodle_left, doodle_rect)
 
-class Entity():
-    screen = pygame.display.set_mode()
-    w, h = screen.get_size()
+    def draw_platform(self):
+        for i in range(10):
+            screen.blit(self.yellow_platform, self.platform_rect[i])
 
-    first_platform_x = w * 48 / 100
-    first_platform_y = h * 4 / 5
+class Entity:
 
     def __init__(self):
         self.background = pygame.image.load('Graphics/background.png').convert_alpha()
-        self.yellow_platform = pygame.image.load('Graphics/yellow_platform.png').convert_alpha()
-
-    def draw_foreground_elements(self):
-        #TODO: Change Entity.first_platform to platform[i] etc.
-        yellow_platform_rect = pygame.Rect(Entity.first_platform_x, Entity.first_platform_y, 225, 65)
-        screen.blit(self.yellow_platform, yellow_platform_rect)
 
     def draw_background_elements(self):
-        border_rect_l = pygame.Rect(0, 0, w/3, h)
-        border_rect_r = pygame.Rect(w*2/3, 0, w/3, h)
-        background_rect = pygame.Rect(w/3, 0, w/3, h)
-        pygame.draw.rect(screen, (0,0,0), border_rect_l)
-        pygame.draw.rect(screen, (0,0,0), border_rect_r)
+        border_rect_l = pygame.Rect(0, 0, w / 3, h)
+        border_rect_r = pygame.Rect(w * 2 / 3, 0, w / 3, h)
+        background_rect = pygame.Rect(w / 3, 0, w / 3, h)
+        pygame.draw.rect(screen, (0, 0, 0), border_rect_l)
+        pygame.draw.rect(screen, (0, 0, 0), border_rect_r)
         screen.blit(self.background, background_rect)
 
-class Main():
+
+class Main:
     def __init__(self):
-        self.background = Entity()
-        self.platform = Entity()
         self.doodle = Doodle()
+        self.background = Entity()
 
     def update(self):
         self.doodle.vertical_movement()
@@ -84,8 +116,7 @@ class Main():
     def draw_elements(self):
         self.background.draw_background_elements()
         self.doodle.draw_doodle()
-        self.platform.draw_foreground_elements()
-
+        self.doodle.draw_platform()
 
 pygame.init()
 screen = pygame.display.set_mode()
